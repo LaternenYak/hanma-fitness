674c111fa697afb63d9874236b222d1ace2b4d88
LaternenYak <breschchris@gmail.com>
2025-06-12 10:17:39 +0200

    Update: neues Design & ein Button

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded",() => {
     let start_input = document.getElementById("start_input");
     let finish_input = document.getElementById("finish_input");
     let today_input = document.getElementById("today_input");
-    let start_btn = document.getElementById("start_btn");
-    let finish_btn = document.getElementById("finish_btn");
     let today_btn = document.getElementById("today_btn");
     let progress_output = document.getElementById("progress_output");
     let toGo_output = document.getElementById("toGo_output");
@@ -13,19 +11,9 @@ document.addEventListener("DOMContentLoaded",() => {
     let finish_weight = null;
     let today_weight = null;
 
-    start_btn.addEventListener("click", () => {
-        start_weight = parseFloat(start_input.value);
-        console.log("Startgewicht: " + start_weight);
-        checkProgress();
-    });
-
-    finish_btn.addEventListener("click", () => {
-        finish_weight = parseFloat(finish_input.value);
-        console.log("Zielgewicht: " + finish_weight);
-        checkProgress();
-    });
-
     today_btn.addEventListener("click", () => {
+        start_weight = parseFloat(start_input.value);
+        finish_weight = parseFloat(finish_input.value);
         today_weight = parseFloat(today_input.value);
         console.log("Heutiges Gewicht: " + today_weight);
         checkProgress();
